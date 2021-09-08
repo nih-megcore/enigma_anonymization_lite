7a7033eecbaf70d3e65e9376991aa0ce4b0511a3
Jeff Stout <stoutjd@nih.gov>
2021-09-08 13:05:02 -0400

    Download is to local drive versus container

--- setup_code/install.sh
+++ setup_code/install.sh
@@ -24,12 +24,12 @@ sudo singularity build ../containers/enigma_prep.sif  singularity_build.def
 
 echo "Downloading defacing code and templates from freesurfer"
 #echo "singularity run wget https://surfer.nmr.mgh.harvard.edu/pub/dist/mri_deface/face.gca.gz"
-singularity exec ${enigma_prep_container} wget https://surfer.nmr.mgh.harvard.edu/pub/dist/mri_deface/face.gca.gz
-singularity exec ${enigma_prep_container} wget https://surfer.nmr.mgh.harvard.edu/pub/dist/mri_deface/talairach_mixed_with_skull.gca.gz
+singularity exec -B $(pwd):$(pwd) ${enigma_prep_container} wget https://surfer.nmr.mgh.harvard.edu/pub/dist/mri_deface/face.gca.gz
+singularity exec -B $(pwd):$(pwd) ${enigma_prep_container} wget https://surfer.nmr.mgh.harvard.edu/pub/dist/mri_deface/talairach_mixed_with_skull.gca.gz
 
 echo "Unzipping defacing templates"
-singularity exec ${enigma_prep_container} gunzip face.gca.gz
-singularity exec ${enigma_prep_container} gunzip talairach_mixed_with_skull.gca.gz
+singularity exec -B $(pwd):$(pwd) ${enigma_prep_container} gunzip face.gca.gz
+singularity exec -B $(pwd):$(pwd) ${enigma_prep_container} gunzip talairach_mixed_with_skull.gca.gz
 
 
 
