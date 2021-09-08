094303599491bd102cfbf8c4ee7cca3050805b8d
Jeff Stout <stoutjd@nih.gov>
2021-09-08 10:52:18 -0400

    Added defacing integration

--- setup_code/install.sh
+++ setup_code/install.sh
@@ -15,6 +15,23 @@ then
 	mkdir ../routines
 fi
 
+enigma_prep_container=../containers/enigma_prep.sif
+
+echo "Admin priveledges are required to build the singularity container:"
+sudo singularity build ../containers/enigma_prep.sif  singularity_build.def
 
 
 
+echo "Downloading defacing code and templates from freesurfer"
+#echo "singularity run wget https://surfer.nmr.mgh.harvard.edu/pub/dist/mri_deface/face.gca.gz"
+singularity exec ${enigma_prep_container} wget https://surfer.nmr.mgh.harvard.edu/pub/dist/mri_deface/face.gca.gz
+singularity exec ${enigma_prep_container} wget https://surfer.nmr.mgh.harvard.edu/pub/dist/mri_deface/talairach_mixed_with_skull.gca.gz
+
+echo "Unzipping defacing templates"
+singularity exec ${enigma_prep_container} gunzip face.gca.gz
+singularity exec ${enigma_prep_container} gunzip talairach_mixed_with_skull.gca.gz
+
+
+
+### STOP
+#singularity exec ${enigma_prep_container} mri_deface 
