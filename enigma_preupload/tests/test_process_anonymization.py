563badc9a60863acc83620e4ef13ce380e2706df
Jeff Stout <stoutjd@nih.gov>
2021-11-18 12:04:03 -0500

    ENH - converted tmpdir to session scoped tempdir_factory

--- enigma_preupload/tests/test_process_anonymization.py
+++ enigma_preupload/tests/test_process_anonymization.py
@@ -20,66 +20,61 @@ keyword_identifiers={'SUBJID': [],
                      'SUBJID_last': [],
                      'DATE': [], 
                      'TASK': []}
-    
 
-def test_datatype_from_template_MEG(tmpdir):
-    #Initialize
-    topdir = tmpdir
-    datatype =  'Meg'
-    #Create directory and dummy files
-    meg_dir = op.join(topdir, 'MEG')
-    os.mkdir(meg_dir)
-    null_files = ['Subj1_rest_100101_02.ds','Subject2_rest_102511_03.ds',
+global null_meg_fnames
+global null_mri_fnames
+null_meg_fnames = ['Subj1_rest_100101_02.ds','Subject2_rest_102511_03.ds',
                   'Subj19_rest_010101_05.ds','Subj20_rest_020202_01.ds']
-    for fname in null_files:
-        out_fname = op.join(meg_dir, fname)
-        Path(out_fname).touch() 
-    
-    #Create dataframe from template
+null_mri_fnames = ['Subj1_T1w.nii', 'Subject2_T1w.nii', 'Subj19_T1w.nii',
+                  'Subj20_T1w.nii']
+
+@pytest.fixture(scope='module')
+def test_setup_meg(tmpdir_factory):
+    meg_dir = tmpdir_factory.mktemp('MEG')
+    for fname in null_meg_fnames:
+        tmp_ = meg_dir.join(fname)
+        tmp_.write('')
+    return meg_dir
+
+@pytest.fixture(scope='module')
+def test_setup_mri(tmpdir_factory):
+    mri_dir = tmpdir_factory.mktemp('MRI') 
+    for fname in null_mri_fnames:
+        tmp_ = mri_dir.join(fname)
+        tmp_.write('')
+    return mri_dir
+
+def test_datatype_from_template_MEG(test_setup_meg):
+    #Initialize
+    topdir = op.dirname(test_setup_meg) #.getbasetemp()
+    datatype =  'Meg'
+    meg_dir = test_setup_meg 
     template = op.join(meg_dir, '{SUBJID}_{TASK}_{DATE}_??.ds')
     datatype_from_template(topdir, interactive=False, template=template, 
                            datatype=datatype.lower())
     meg_csv = op.join(topdir, 'meg_dframe.csv')
     assert op.exists(meg_csv)
     meg_dframe = pd.read_csv(meg_csv)
-    assert len(meg_dframe) == len(null_files)
+    assert len(meg_dframe) == len(null_meg_fnames)
 
     
-def test_datatype_from_template_MRI(tmpdir):
+def test_datatype_from_template_MRI(test_setup_mri):
     #Initialize
-    topdir = tmpdir
+    topdir = op.dirname(test_setup_mri) 
     datatype =  'MRI'
-    #Create directory and dummy files
-    mri_dir = op.join(topdir, 'MRI')
-    os.mkdir(mri_dir)
-    null_files = ['Subj1_T1w.nii', 'Subject2_T1w.nii', 'Subj19_T1w.nii',
-                  'Subj20_T1w.nii']
-    for fname in null_files:
-        out_fname = op.join(mri_dir, fname)
-        Path(out_fname).touch() 
-    
+    mri_dir = test_setup_mri
     template = op.join(mri_dir, '{SUBJID}_T1w.nii')
     datatype_from_template(topdir, interactive=False, template=template, 
                            datatype=datatype.lower())
     mri_csv = op.join(topdir, 'mri_dframe.csv')
     assert op.exists(mri_csv)
     mri_dframe = pd.read_csv(mri_csv)
-    assert len(mri_dframe)==len(null_files)
+    assert len(mri_dframe)==len(null_mri_fnames)
     
-def test_dframe_from_template(tmpdir):
-    topdir = tmpdir
+def test_dframe_from_template(test_setup_mri):
     datatype =  'MRI'
-    
-    mri_dir = op.join(topdir, 'MRI')
+    mri_dir = test_setup_mri
     template = op.join(mri_dir, '{SUBJID}_T1w.nii')
-    os.mkdir(mri_dir)
-    
-    null_files = ['Subj1_T1w.nii', 'Subject2_T1w.nii', 'Subj19_T1w.nii',
-                  'Subj20_T1w.nii']
-    for fname in null_files:
-        out_fname = op.join(mri_dir, fname)
-        Path(out_fname).touch() 
-        
     dframe = _dframe_from_template(template, keyword_identifiers, 
                                    datatype=datatype.lower())
     assert len(dframe)==4    
